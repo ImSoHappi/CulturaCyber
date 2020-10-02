eeb597735ce94cf7e84f04329227a11651ede15f
samuel <samuelpguzman@hotmail.com>
2020-10-02 17:20:37 -0300

    base de datos

--- culturacyberBase/settings.py
+++ culturacyberBase/settings.py
@@ -49,6 +49,7 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
