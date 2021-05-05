654e737b3b56521751cf394a09eacff44e71e2a2
Samuel Peña <43582318+ImSoHappi@users.noreply.github.com>
2021-05-04 20:39:21 -0400

    Update settings.py

--- culturacyberBase/settings.py
+++ culturacyberBase/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5en&e*8k1xa$+@%av=f%g3&mcyxuu5200+*$o2(^l%a(yjku)i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
