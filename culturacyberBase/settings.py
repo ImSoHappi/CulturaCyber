d1af8d363ceffa187c35bfab822633f2bed169c4
Samuel Peña <43582318+ImSoHappi@users.noreply.github.com>
2021-05-04 20:28:28 -0400

    Update settings.py

--- culturacyberBase/settings.py
+++ culturacyberBase/settings.py
@@ -140,6 +140,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'culturacyberCore/media/')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'SMTP.Office365.com'
-EMAIL_HOST_USER = 'culturacyber@cybertrust.cl'
-EMAIL_HOST_PASSWORD = 'WCS&qkmUEYAX'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
